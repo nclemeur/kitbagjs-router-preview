56c632d95ab1f9cd38a4d1417ba345bf4d4ab3e6
nclemeur <nclemeur@gmail.com>
2024-12-18 16:18:50 +1000

    cleanup

--- src/views/settings/SettingsKeysView.vue
+++ src/views/settings/SettingsKeysView.vue
@@ -1,7 +1,5 @@
 <script lang="ts" setup>
 import { useRoute } from '@kitbag/router';
-
-const settingsRoute = useRoute('settings')
 const route = useRoute('settings.keys')
 </script>
 
@@ -9,9 +7,7 @@ const route = useRoute('settings.keys')
   <div class="settings-keys-view">
     <h1>Keys</h1>
     <p>Keys also implements useRoute for additional layering of query params with parent.</p>
-
-    id: {{ route.params.id }} {{  settingsRoute.params.id }}
-    sort: {{ route.params.sort }}
+    
     <select v-model="route.params.sort">
       <option value="asc">Ascending</option>
       <option value="desc">Descending</option>
