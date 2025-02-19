c399d43dd8b84f9d10a8347210ebe236d2ff51c3
nclemeur <nclemeur@gmail.com>
2025-02-19 16:42:24 +1000

    Example with defaults

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,33 @@
-import { createRouter, createRoute, withParams, unionOf, withDefault } from "@kitbag/router";
+import { createRouter, createRoute, withParams, unionOf, withDefault, createParam, ParamExtras } from "@kitbag/router";
 import HomeView from "../views/HomeView.vue";
 import { defineAsyncComponent } from "vue";
 import LoginView from "../views/LoginView.vue";
 
+
+export const stringListParam = 
+  createParam({
+    get: (value: string, extras: ParamExtras) => {
+      if (typeof value !== 'string') {
+        return extras.invalid('Expected a string')
+      }
+
+      return value.split('|')
+    },
+    set: (value: string[]) => {
+      if (!Array.isArray(value)) {
+        return ''
+      }
+      return value.join('|')
+    },
+  })
+
 const home = createRoute({ 
   name: 'home', 
   path: '/', 
+  query: withParams('fruits=[?fruits]', { 
+    fruits: withDefault(stringListParam, ['apple']) 
+    // fruits: stringListParam // <= with no default it is working!
+  }),
   component: HomeView 
 })
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,27 @@
+<script lang="ts" setup>
+import { useRoute } from '@kitbag/router';
+
+const route = useRoute('home')
+
+const onClearFruits = () => {
+  route.params.fruits = [];  
+}
+const onUseOrangesAndBananas = () => {
+  route.params.fruits = ['oranges', 'bananas'];  
+}
+</script>
 <template>
   <div class="home-view">
     <h1>Home</h1>
     <p>Welcome to our tiny demo of kitbag/router!</p>
+
+ <span>Fruits: {{route.params.fruits}}</span>
+
+ <button @click="onClearFruits">Clear Fruits</button>
+ <button @click="onUseOrangesAndBananas">Use Oranges And Bananas</button>
+
     <router-link to="https://router.kitbag.dev" target="_blank">Documentation</router-link>
+
+   
   </div>
 </template>
